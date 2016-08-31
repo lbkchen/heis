70305c43dc6fe90bf4a974ea01ef38546e220dd8
lbkchen <lbkchen@gmail.com>
2016-08-30 17:33:36 -0700

    Added main menu. Next we will have Michael Bay style animations

--- iOS/heis/heis/heis/GameScene.swift
+++ iOS/heis/heis/heis/GameScene.swift
@@ -9,14 +9,42 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    required init(coder aDecoder: NSCoder) {
+        fatalError("NSCoder not supported")
+    }
+    
+    override init(size: CGSize) {
+        super.init(size: size)
+        // Default anchorPoint is actually just this
+        anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        
+        let background = SKSpriteNode(imageNamed: "background")
+        // Same as above default anchorpoint is just 0.5, 0.5
+        // See this guide for further reference on anchor points: 
+        // http://www.garethelms.org/2014/06/help-with-spritekit-position-and-anchorpoint/
+        background.anchorPoint = CGPointMake(0.5, 0.5)
+        addChild(background)
+        
+        // Create title
+        let title = SKSpriteNode(imageNamed: "logo")
+        title.position = CGPointMake(0, 160)
+        background.addChild(title)
+        
+        // Create menu buttons
+        let menuLayer = SKNode()
+        menuLayer.position = CGPointMake(0, -60)
+        background.addChild(menuLayer)
+        displayMenuButtons(menuLayer, spacing: CGFloat(10.0))
+    }
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        
-        self.addChild(myLabel)
+//        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+//        myLabel.text = "Hello, World!"
+//        myLabel.fontSize = 45
+//        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+//        
+//        self.addChild(myLabel)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -25,7 +53,7 @@ class GameScene: SKScene {
         for touch in touches {
             let location = touch.locationInNode(self)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            let sprite = SKSpriteNode(imageNamed:"menu_buttons_help")
             
             sprite.xScale = 0.5
             sprite.yScale = 0.5
@@ -41,5 +69,32 @@ class GameScene: SKScene {
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    func displayMenuButtons(layer: SKNode, spacing: CGFloat) {
+        let buttonOne = SKSpriteNode(imageNamed: "menu_buttons_create_game")
+        let buttonTwo = SKSpriteNode(imageNamed: "menu_buttons_join_game")
+        let buttonThree = SKSpriteNode(imageNamed: "menu_buttons_help")
+        let buttonHeight = buttonTwo.size.height
+        buttonOne.position = CGPointMake(0, buttonHeight + spacing)
+        buttonThree.position = CGPointMake(0, -1 * (buttonHeight + spacing))
+        buttonOne.alpha = 0
+        buttonTwo.alpha = 0
+        buttonThree.alpha = 0
+        
+        layer.addChild(buttonOne)
+        layer.addChild(buttonTwo)
+        layer.addChild(buttonThree)
+        for button in [buttonOne, buttonTwo, buttonThree] {
+            button.runAction(easeInDown(button, distance: 10, duration: 0.2))
+            print("run")
+        }
+    }
+    
+    func easeInDown(node: SKNode, distance: CGFloat, duration: CGFloat) -> SKAction {
+        let destPoint = CGPointMake(node.position.x, node.position.y - distance)
+        let moveAction = SKAction.moveTo(destPoint, duration: NSTimeInterval(duration))
+        let fadeAction = SKAction.fadeAlphaTo(1.0, duration: NSTimeInterval(duration))
+        return SKAction.group([moveAction, fadeAction])
     }
 }
--- iOS/heis/heis/heis/GameViewController.swift
+++ iOS/heis/heis/heis/GameViewController.swift
@@ -15,20 +15,18 @@ class GameViewController: UIViewController, CLLocationManagerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let scene = GameScene(fileNamed:"GameScene") {
-            // Configure the view.
-            let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
-            
-            skView.presentScene(scene)
-        }
+        let skView = view as! SKView
+        
+        let scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
+        scene.size = skView.bounds.size
+
+        skView.multipleTouchEnabled = false
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+//        skView.ignoresSiblingOrder = true
+        skView.presentScene(scene)
+        
     }
 
     override func shouldAutorotate() -> Bool {
